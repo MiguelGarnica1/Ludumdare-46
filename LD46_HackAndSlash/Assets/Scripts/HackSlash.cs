af580535d143f9687324acec10f102658fda53db
Miguel Garnica <mgarnica123@aol.com>
2020-04-17 22:22:25 -0700

    Health and Axe
    Added:
        - HackSlash.cs ( attac script for axe)
        - Health.cs
        - Axe

---
+++ LD46_HackAndSlash/Assets/Scripts/HackSlash.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HackSlash : MonoBehaviour
+{
+    private KeyCode attack;
+    private Vector2 mousePos;
+    private Vector3 screenPos;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        //Get the Screen positions of the object
+        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+
+        //Get the Screen position of the mouse
+        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        //Get the angle between the points
+        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+
+        //Ta Daaa
+        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle + 90));
+    }
+
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+    }
+
+
+
+}
---
+++ LD46_HackAndSlash/Assets/Scripts/Health.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    public float max_health;
+    private float hp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+
+    public void Damage(float damage)
+    {
+        hp -= damage;
+    }
+
+
+    public float GetHealth()
+    {
+        return hp;
+    }
+
+}
