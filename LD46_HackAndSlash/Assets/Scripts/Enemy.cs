1229e987fbf70e2e2b1ce63aa4c8f2f7911ce2d0
HowardDful123 <HowardDful123@gmail.com>
2020-04-18 00:31:43 -0700

    -Enemy change color when attack(not implemted yet)

--- LD46_HackAndSlash/Assets/Scripts/Enemy.cs
+++ LD46_HackAndSlash/Assets/Scripts/Enemy.cs
@@ -8,10 +8,55 @@ public abstract class Enemy : MonoBehaviour
     public float moveSpeed;
     public float damage;
 
+    [SerializeField]
     protected float stoppingPoint;
 
-    public abstract void moveTowardTarget(Transform target);
     public abstract void Die();
-    public abstract void Attack(int damage);
+    public abstract void Attack(float damage);
+
+    protected Transform target;
+
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        Die();
+    }
+
+    protected void Update()
+    {
+        if (Vector2.Distance(transform.position, target.position) > stoppingPoint)
+        {
+            moveTowardTarget(target);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Attack(damage);
+        }
+    }
+
+    protected void moveTowardTarget(Transform target)
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+    }
+
+    /*
+     * Turn Red when it is attacked
+     */
+    protected void ChangeColor()
+    {
+        float waitTime = 0.30f;//Time until color is return back to normal
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        Color defaultColor = sr.color;
+        sr.color = new Color(1f, 0, 0, .7f);
+
+        while (waitTime > 0)
+        {
+            waitTime -= Time.deltaTime;
+        }
+
+        sr.color = defaultColor;
+    }
 
 }
