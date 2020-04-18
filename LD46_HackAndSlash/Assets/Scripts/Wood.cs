d78e72438f8e21f0269c8051c74d6c7779398ca6
EvanStJohn <46731712+EvanStJohn@users.noreply.github.com>
2020-04-17 21:29:11 -0700

    add Generator and log inventory

---
+++ LD46_HackAndSlash/Assets/Scripts/Wood.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wood : MonoBehaviour
+{
+    public float speed;
+    public GameObject p1;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            p1.GetComponent<Inventory>().inventory++;
+            Destroy(gameObject);
+        }
+
+        if (collision.gameObject.CompareTag("Generator"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
