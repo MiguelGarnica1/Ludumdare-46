2e6aa258292a47e4553df17cde853f48905227e9
Miguel Garnica <mgarnica123@aol.com>
2020-04-19 22:32:06 -0700

    menu
    Menu and theme

---
+++ LD46_HackAndSlash/Assets/Scripts/Menu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Menu : MonoBehaviour
+{
+
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(audioSource.volume < 1)
+        {
+            audioSource.volume += 0.1f * Time.deltaTime;
+        }
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Miguel");
+    }
+}
